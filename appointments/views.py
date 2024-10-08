5b16b20b7ae9b98ac8b52effe284f2d3a7f1906d
Amy Norfolk Tate <amynorfolk2@gmail.com>
2024-10-08 17:00:48 +0000

    add function for services to views.py

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Service, Appointment
+from .forms import AppointmentForm
 # Create your views here.
+
+def service_list(request):
+    services = Service.objects.all()
+    return render(request, 'appointments/service_list.html', {'services': services})
