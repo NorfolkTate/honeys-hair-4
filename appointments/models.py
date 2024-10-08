bd8b286bed9fbe0303347c8c3db64666fd19b8d9
Amy Norfolk Tate <amynorfolk2@gmail.com>
2024-10-08 09:40:15 +0000

    add model for services

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Appointment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    is_confirmed = models.BooleanField(default=False)
+
+
