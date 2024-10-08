e8b917ce997fd9e0e5d8ae3b31777a5459bde206
Amy Norfolk Tate <amynorfolk2@gmail.com>
2024-10-08 09:48:15 +0000

    add success messages to models

--- appointments/models.py
+++ appointments/models.py
@@ -8,10 +8,16 @@ class Service(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.name
+
 class Appointment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     date = models.DateTimeField()
     is_confirmed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Thanks for booking! Your appointment for {self.user.username} is on {self.date}"
 
 
