0d4cad9e0fd4aa2bc556920cdf36e4d3498614d9
Amy Norfolk Tate <amynorfolk2@gmail.com>
2024-10-09 09:43:03 +0000

    add function to book appointment

--- appointments/views.py
+++ appointments/views.py
@@ -6,3 +6,20 @@ from .forms import AppointmentForm
 def service_list(request):
     services = Service.objects.all()
     return render(request, 'appointments/service_list.html', {'services': services})
+
+def book_appointment(request, service_id):
+    service = get_object_or_404(Service, id=service_id)
+
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            appointment = form.save(commit=False)
+            appointment.user = request.user
+            appointment.service = service
+            appointment.save()
+            return redirect('my_appointments')
+    else:
+        form = AppointmentForm()
+
+    return render(request, 'appointments/book_appointment.html', {'service': service, 'form': form})
+
