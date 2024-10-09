7db64dffefeb2752043e5ab4e428f8423f241395
Amy Norfolk Tate <amynorfolk2@gmail.com>
2024-10-09 10:32:58 +0000

    add book appointment and service list html pages and add urls

--- honeys_hair/urls.py
+++ honeys_hair/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('services/', views.service_list, name='service_list'),
+    path('book/<int:service_id>/', views.book_appointment, name='book_appointment'),
 ]
