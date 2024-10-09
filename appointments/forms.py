e4ec478cac5f17d8af96bf2930aad16191dc77ab
Amy Norfolk Tate <amynorfolk2@gmail.com>
2024-10-09 10:53:29 +0000

    fix capitalization error in forms.py

--- appointments/forms.py
+++ appointments/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Appointment
 
-class AppointmentForm(forms.modelForm):
+class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Appointment
         fields = ['date', 'is_confirmed']
